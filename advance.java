124fa9ba7a1633439c7c99cb2308a683e2a45c42
Daniel Peter <peterdanielfun24@gmail.com>
2024-04-10 07:52:52 +0100

    learning more

---
+++ advance.java
@@ -0,0 +1,13 @@
+public class advance {
+    public static void main(String[] args) {
+        newDemo newObj = new newDemo();
+        System.out.println(newObj.value);
+        System.out.println(newObj.words);
+
+        newObj.value = 20;
+        newObj.words = "Keep learning and never stop until you are good like never \"before\"";
+
+        System.out.println(newObj.value);
+        System.out.println(newObj.words);
+    }
+}
---
+++ newDemo.java
@@ -0,0 +1,4 @@
+public class newDemo {
+    int value = 30;
+    String words = "Java is fun and powerful";
+}
